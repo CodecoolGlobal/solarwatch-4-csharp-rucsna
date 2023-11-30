6472e61a437aeee7f025ae2af6931a965d0686b3
Anna Pomichal <rucsna@gmail.com>
2023-11-30 20:49:09 +0100

    feat: create SolarData model class

---
+++ SolarWatch/SolarWatch/Model/SolarData.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarWatch.Model;
+
+public class SolarData
+{
+    public string? City { get; set; }
+    public DateTime Date { get; set; }
+    public string? Sunrise { get; set; }
+    public string? Sunset { get; set; }
+}
