7b270b47e88e3ab8e359d5d209d74bdbeaee947d
Anna Pomichal <rucsna@gmail.com>
2023-11-30 20:54:41 +0100

    feat: implement ICityDataProvider interface

---
+++ SolarWatch/SolarWatch/Service/GeocodingApi.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using SolarWatch.Client;
+
+namespace SolarWatch.Service;
+
+public class GeocodingApi : ICityDataProvider
+{
+    private readonly ISolarWatchClient _solarWatchClient;
+    private readonly ILogger<GeocodingApi> _logger;
+
+    public GeocodingApi(ISolarWatchClient solarWatchClient, ILogger<GeocodingApi> logger)
+    {
+        _solarWatchClient = solarWatchClient;
+        _logger = logger;
+    }
+    
+    public Task<string> GetCityDataAsync(string cityName)
+    {
+        throw new NotImplementedException();
+    }
+}
