ba2172b1714d4af72714e74e6a84ce1f9f249515
Anna Pomichal <rucsna@gmail.com>
2023-11-30 21:25:53 +0100

    feat: implement ISolarJsonProcessor

---
+++ SolarWatch/SolarWatch/Processors/SolarJsonProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using SolarWatch.Model;
+
+namespace SolarWatch.Processors;
+
+public class SolarJsonProcessor : ISolarJsonProcessor
+{
+    public SolarData Process(string data, string city, DateTime date)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        JsonElement results = json.RootElement.GetProperty("results");
+
+        SolarData solarResult = new SolarData
+        {
+            City = city,
+            Date = date,
+            Sunrise = results.GetProperty("sunrise").ToString(),
+            Sunset = results.GetProperty("sunset").ToString()
+        };
+
+        return solarResult;
+    }
+}
