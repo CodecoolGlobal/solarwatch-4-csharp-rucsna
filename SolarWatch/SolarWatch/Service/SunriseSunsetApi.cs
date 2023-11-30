ee7ca7475a1ef165482a24c029a3dec4213b4193
Anna Pomichal <rucsna@gmail.com>
2023-11-30 21:24:19 +0100

    feat: implement GetSolarDataAsync method

--- SolarWatch/SolarWatch/Service/SunriseSunsetApi.cs
+++ SolarWatch/SolarWatch/Service/SunriseSunsetApi.cs
@@ -16,6 +16,14 @@ public class SunriseSunsetApi : ISolarDataProvider
 
     public async Task<string> GetSolarDataAsync(float lat, float lon, DateTime date)
     {
-        throw new NotImplementedException();
+        var formattedDate = date.ToString("yyyy-MM-dd");
+        
+        var url = $"https://api.sunrise-sunset.org/json?lat={lat}&lng={lon}&date={formattedDate}";
+       
+        _logger.LogInformation("Calling Sunrise/Sunset API from url: {url}", url);
+
+        var client = _solarWatchClient.GetClient();
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
     }
 }
