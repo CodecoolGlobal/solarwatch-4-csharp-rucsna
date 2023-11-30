dd69a0815c94e42f8bdcd9ca3b4d38d8f50ed252
Anna Pomichal <rucsna@gmail.com>
2023-11-30 20:50:24 +0100

    feat: create http client class

---
+++ SolarWatch/SolarWatch/Client/SolarWatchClient.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarWatch.Client;
+
+public class SolarWatchClient : ISolarWatchClient
+{
+    private static readonly HttpClient Client = new HttpClient();
+
+    public HttpClient GetClient()
+    {
+        return Client;
+    }
+}
