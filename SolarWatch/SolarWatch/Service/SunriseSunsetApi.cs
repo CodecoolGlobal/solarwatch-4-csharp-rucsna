25c8efb57f438258b5b8d099443f5855704c752e
Anna Pomichal <rucsna@gmail.com>
2023-11-30 21:14:18 +0100

    feat: implement ISolarDataProvider interface

---
+++ SolarWatch/SolarWatch/Service/SunriseSunsetApi.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using SolarWatch.Client;
+
+namespace SolarWatch.Service;
+
+public class SunriseSunsetApi : ISolarDataProvider
+{
+    private readonly ISolarWatchClient _solarWatchClient;
+    private readonly ILogger<SunriseSunsetApi> _logger;
+
+    public SunriseSunsetApi(ISolarWatchClient solarWatchClient, ILogger<SunriseSunsetApi> logger)
+    {
+        _solarWatchClient = solarWatchClient;
+        _logger = logger;
+    }
+
+    public async Task<string> GetSolarDataAsync(float lat, float lon, DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+}
