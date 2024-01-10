5388d6ee5ca13fbebe3392b57592b03ecc6b091f
Anna Pomichal <rucsna@gmail.com>
2024-01-10 09:12:40 +0100

    refactor: add repository layer to Controller

--- SolarWatch/SolarWatch/Controllers/SolarWatchController.cs
+++ SolarWatch/SolarWatch/Controllers/SolarWatchController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SolarWatch.Model;
-using SolarWatch.Processors;
+using SolarWatch.Service.Processors;
 using SolarWatch.Service;
+using SolarWatch.Service.Repository;
 
 namespace SolarWatch.Controllers;
 
@@ -10,39 +12,30 @@ namespace SolarWatch.Controllers;
 public class SolarWatchController : ControllerBase
 {
     private readonly ILogger<SolarWatchController> _logger;
-    private readonly ICityDataProvider _cityDataProvider;
-    private readonly ISolarDataProvider _solarDataProvider;
-    private readonly ICityJsonProcessor _cityJsonProcessor;
-    private readonly ISolarJsonProcessor _solarJsonProcessor;
+    private readonly ISolarWatchRepository _repository;
 
-    public SolarWatchController(ILogger<SolarWatchController> logger, ICityDataProvider cityDataProvider, ISolarDataProvider solarDataProvider, ICityJsonProcessor cityJsonProcessor, ISolarJsonProcessor solarJsonProcessor)
+    public SolarWatchController(ILogger<SolarWatchController> logger, ISolarWatchRepository repository)
     {
         _logger = logger;
-        _cityDataProvider = cityDataProvider;
-        _solarDataProvider = solarDataProvider;
-        _cityJsonProcessor = cityJsonProcessor;
-        _solarJsonProcessor = solarJsonProcessor;
+        _repository = repository;
     }
     
-    [HttpGet("GetSunrise_Sunset")]
+    [HttpGet("GetSunrise_Sunset")/*, Authorize(Roles="User, Admin")*/]
     public async Task<ActionResult<SolarData>> GetAsync(string cityName, DateTime date)
     {
         try
         {
-            var cityData = await _cityDataProvider.GetCityDataAsync(cityName);
-            
-            var lat = _cityJsonProcessor.Process(cityData).Latitude;
-            var lon = _cityJsonProcessor.Process(cityData).Longitude;
-
-            var solarData = await _solarDataProvider.GetSolarDataAsync(lat, lon, date);
-
-            return Ok(_solarJsonProcessor.Process(solarData, cityName, date));
+            var result = await _repository.GetDataAndAddToDbAsync(cityName, date);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound("No solar data could be found.");
         }
         catch (Exception e)
         {
             _logger.LogError(e, "Error getting solar data");
-            return NotFound("Error getting solar data");
+            return BadRequest("Error getting solar data");
         }
-        
     }
 }
