250ea0c6ed7e30c4972fbb4c7f76ab2fc19deff4
Anna Pomichal <rucsna@gmail.com>
2024-01-10 09:00:33 +0100

    feat: create CityJsonProcessor.cs

---
+++ SolarWatch/SolarWatch/Service/Processors/CityJsonProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using SolarWatch.Model;
+
+namespace SolarWatch.Service.Processors;
+
+public class CityJsonProcessor : ICityJsonProcessor
+{
+    private readonly ILogger<CityJsonProcessor> _logger;
+
+    public CityJsonProcessor(ILogger<CityJsonProcessor> logger)
+    {
+        _logger = logger;
+    }
+
+    public City Process(string data)
+    {
+        var json = JsonDocument.Parse(data);
+        var response = json.RootElement.EnumerateArray().FirstOrDefault();
+        var cityCoordinate = new Coordinate
+        {
+            Latitude = response.GetProperty("lat").GetSingle(),
+            Longitude = response.GetProperty("lon").GetSingle()
+        };
+        
+        var city = new City
+        {
+            CityId = Guid.NewGuid(),
+            Name = response.GetProperty("name").ToString(),
+            Coordinate = cityCoordinate,
+            Country = response.GetProperty("country").ToString(),
+            State = response.TryGetProperty("state", out var stateElement) ? stateElement.GetString() : string.Empty
+        };
+        _logger.LogInformation("New city is successfully created.");
+
+        return city;
+    }
+}
