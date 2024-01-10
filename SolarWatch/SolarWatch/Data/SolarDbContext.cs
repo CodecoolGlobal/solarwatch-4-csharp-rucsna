5496a7c48995274aeed1ac6fcfc9fa82e9b25697
Anna Pomichal <rucsna@gmail.com>
2024-01-10 09:01:17 +0100

    feat: create SolarDbContext.cs

---
+++ SolarWatch/SolarWatch/Data/SolarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatch.Model;
+
+namespace SolarWatch.Data;
+
+public class SolarDbContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Coordinate> Coordinates { get; set; }
+    public DbSet<SolarData> SolarData { get; set; }
+
+    public SolarDbContext(DbContextOptions<SolarDbContext> options)
+        : base(options)
+    {
+    }
+}
