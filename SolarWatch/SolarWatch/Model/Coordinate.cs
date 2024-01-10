8251563d1005bcfec831053a1ec4f46ecbf2cbe5
Anna Pomichal <rucsna@gmail.com>
2024-01-10 08:49:57 +0100

    feat: create Coordinate.cs

---
+++ SolarWatch/SolarWatch/Model/Coordinate.cs
@@ -0,0 +1,12 @@
+﻿namespace SolarWatch.Model;
+
+public class Coordinate
+{
+    public Guid CoordinateId { get; set; }
+    public float Latitude { get; init; }
+    public float Longitude { get; init; }
+
+    public Coordinate()
+    {
+    }
+}
