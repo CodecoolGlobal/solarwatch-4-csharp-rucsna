084e04abe4b07f51649c0f6e73d685bbe10dc9ce
Anna Pomichal <rucsna@gmail.com>
2024-01-10 09:02:11 +0100

    feat: create ISolarWatchRepository.cs

---
+++ SolarWatch/SolarWatch/Service/Repository/ISolarWatchRepository.cs
@@ -0,0 +1,8 @@
+﻿using SolarWatch.Model;
+
+namespace SolarWatch.Service.Repository;
+
+public interface ISolarWatchRepository
+{
+    Task<SolarData?> GetDataAndAddToDbAsync(string cityName, DateTime date);
+}
