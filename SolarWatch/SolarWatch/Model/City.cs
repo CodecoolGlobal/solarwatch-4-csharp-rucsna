6379b14541bf2a0584d5f5f553c31ce704c16043
Anna Pomichal <rucsna@gmail.com>
2024-01-10 08:50:19 +0100

    refactor: rename City class and add properties

---
+++ SolarWatch/SolarWatch/Model/City.cs
@@ -0,0 +1,14 @@
+﻿namespace SolarWatch.Model;
+
+public class City
+{
+    public Guid CityId { get; set; }
+    public string Name { get; set; }
+    public Coordinate Coordinate { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+
+    public City()
+    {
+    }
+}
--- SolarWatch/SolarWatch/Model/CityData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SolarWatch.Model;
-
-public class CityData
-{
-    public string? Name { get; set; }
-    public float Latitude { get; set; }
-    public float Longitude { get; set; }
-}
