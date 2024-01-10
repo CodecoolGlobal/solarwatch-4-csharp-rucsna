bc6b84dff3ebe8d63cb8fe2ec3d3d300197500ed
Anna Pomichal <rucsna@gmail.com>
2024-01-10 09:13:31 +0100

    chore: add services, Identity and Authentication

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,20 +1,24 @@
-using SolarWatch.Client;
-using SolarWatch.Processors;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SolarWatch.Service;
+using SolarWatch.Service.Repository;
+using SolarWatch.Data;
+using SolarWatch.Service.Authentication;
+using SolarWatch.Service.Processors;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ISolarWatchClient, SolarWatchClient>();
-builder.Services.AddScoped<ICityDataProvider, GeocodingApi>();
-builder.Services.AddScoped<ISolarDataProvider, SunriseSunsetApi>();
-builder.Services.AddScoped<ICityJsonProcessor, CityJsonProcessor>();
-builder.Services.AddScoped<ISolarJsonProcessor, SolarJsonProcessor>();
+AddServices();
+ConfigureSwagger();
+AddDbContext();
+AddAuthentication();
+AddIdentity();
 
 var app = builder.Build();
 
@@ -27,8 +31,152 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
+AddRoles();
+AddAdmin();
+
 app.Run();
+
+
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddHttpClient();
+    builder.Services.AddScoped<ICityDataProvider, GeocodingApi>();
+    builder.Services.AddScoped<ISolarDataProvider, SunriseSunsetApi>();
+    builder.Services.AddScoped<ICityJsonProcessor, CityJsonProcessor>();
+    builder.Services.AddScoped<ISolarJsonProcessor, SolarJsonProcessor>();
+    builder.Services.AddScoped<ISolarWatchRepository, SolarWatchRepository>();
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });  
+    });
+}
+
+void AddDbContext()
+{
+    builder.Services.AddDbContext<SolarDbContext>(
+        options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+    builder.Services.AddDbContext<UsersContext>(
+        options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+}
+
+void AddAuthentication()
+{
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JwtSettings:ValidIssuer"],
+                ValidAudience = builder.Configuration["JwtSettings:ValidAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["JwtIssuerSigningKey"] ?? string.Empty)
+                ),
+            };
+        });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<UsersContext>();
+}
+
+void AddRoles()
+{
+    using var scope = app.Services.CreateScope();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var tAdmin = CreateAdminRole(roleManager);
+    tAdmin.Wait();
+
+    var tUser = CreateUserRole(roleManager);
+    tUser.Wait();
+}
+
+async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole(builder.Configuration["RoleSettingsAdminRole"]!));
+}
+
+async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
+{
+    await roleManager.CreateAsync(new IdentityRole(builder.Configuration["RoleSettings:UserRole"]!));
+}
+
+void AddAdmin()
+{
+    var tAdmin = CreateAdminIfNotExists();
+    tAdmin.Wait();
+}
+
+async Task CreateAdminIfNotExists()
+{
+    using var scope = app.Services.CreateScope();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var adminInDb = await userManager.FindByEmailAsync("admin@admin.com");
+    if (adminInDb == null)
+    {
+        var admin = new IdentityUser { UserName = "admin", Email = "admin@admin.com"};
+        var adminCreated = await userManager.CreateAsync(admin, "admin123");
+
+        if (adminCreated.Succeeded)
+        {
+            await userManager.AddToRoleAsync(admin, "Admin");
+        }
+    }
+}
