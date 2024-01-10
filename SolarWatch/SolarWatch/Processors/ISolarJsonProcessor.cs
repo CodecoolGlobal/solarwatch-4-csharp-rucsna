da44bf4743972c4ffd780ec6b69831c2fed2f52f
Anna Pomichal <rucsna@gmail.com>
2024-01-10 08:57:10 +0100

    refactor: relocate ISolarJsonProcessor.cs, adjust namespace

--- SolarWatch/SolarWatch/Processors/ISolarJsonProcessor.cs
+++
@@ -1,8 +0,0 @@
-﻿using SolarWatch.Model;
-
-namespace SolarWatch.Processors;
-
-public interface ISolarJsonProcessor
-{
-    SolarData Process(string data, string city, DateTime date);
-}
---
+++ SolarWatch/SolarWatch/Service/Processors/ISolarJsonProcessor.cs
@@ -0,0 +1,8 @@
+﻿using SolarWatch.Model;
+
+namespace SolarWatch.Service.Processors;
+
+public interface ISolarJsonProcessor
+{
+    SolarData? Process(string data, DateTime date);
+}
