8d900b9325c7da0cde21b9320d9a52dc7af8aadf
Anna Pomichal <rucsna@gmail.com>
2024-01-12 11:30:46 +0100

    feat: create ICityService interface

---
+++ SolarWatch/SolarWatch/Service/Repository/ICityService.cs
@@ -0,0 +1,10 @@
+﻿using SolarWatch.Model;
+
+namespace SolarWatch.Service.Repository;
+
+public interface ICityService
+{
+    Task<IEnumerable<City>?> GetCityByNameAsync(string cityName);
+    //Task<IEnumerable<City>?> GetAllCitiesAsync();
+    //Task<City?> CreateCityAsync(Guid id, string name, Coordinate coordinate, string country, string state);
+}
