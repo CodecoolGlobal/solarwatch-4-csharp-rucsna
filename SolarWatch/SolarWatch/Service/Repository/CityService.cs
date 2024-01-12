371b36aa61a41f1a886809cdbf7c9bca2b850c8d
Anna Pomichal <rucsna@gmail.com>
2024-01-12 11:31:24 +0100

    feat: implement ICityService GetCityByName async method

---
+++ SolarWatch/SolarWatch/Service/Repository/CityService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
+using SolarWatch.Model;
+
+namespace SolarWatch.Service.Repository;
+
+public class CityService : ICityService
+{
+    private readonly SolarDbContext _dbContext;
+    private readonly ILogger<CityService> _logger;
+
+    public CityService(SolarDbContext dbContext, ILogger<CityService> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<IEnumerable<City>?> GetCityByNameAsync(string cityName)
+    {
+        var cityList = await _dbContext.Cities
+            .Include(c => c.Coordinate)
+            .Where(city => city.Name.ToLower() == cityName)
+            .ToListAsync();
+        return cityList.Count < 1 ? null : cityList;
+    }
+
+    
+}
